719e9519b530efe876edf39896c5cf99b65601fa
Vorobeva <passant.dlm@gmail.com>
2021-03-02 21:58:24 +0100

    Initial commit

---
+++ app/src/main/java/com/github/ovorobeva/wordstostudy/AppWidget.java
@@ -0,0 +1,130 @@
+package com.github.ovorobeva.wordstostudy;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.Calendar;
+
+import static com.github.ovorobeva.wordstostudy.Words.getWords;
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link ConfigureActivity ConfigureActivity}
+ */
+public class AppWidget extends AppWidgetProvider {
+
+    private static final String ACTION_SCHEDULED_UPDATE = "android.appwidget.action.ACTION_SCHEDULED_UPDATE";
+    //todo: to make "words" constant in string.xml
+    static private String text = "Words";
+    private PendingIntent service = null;
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        // Construct the RemoteViews object
+
+        text = getWords(context);
+        Log.d(AppWidget.class.getCanonicalName() + ".updateAppWidget", "Getword called: New text value for the widget ID " + appWidgetId + " is: " + text);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+        views.setTextViewText(R.id.appwidget_text, text);
+        Log.d(AppWidget.class.getCanonicalName() + ".updateAppWidget", "New text set to the widget ID " + appWidgetId + ". The new word is: " + text);
+
+
+        // Opens the config activity by click on widget
+        Intent configIntent = new Intent(context, ConfigureActivity.class);
+        configIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_CONFIGURE);
+        configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+        PendingIntent pIntent = PendingIntent.getActivity(context, appWidgetId, configIntent, 0);
+        views.setOnClickPendingIntent(R.id.main_layout, pIntent);
+        //todo: to fix back button
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+        Log.d(AppWidget.class.getCanonicalName() + ".updateAppWidget", "Widget ID " + appWidgetId + " is updated");
+
+    }
+//todo: to fix the schedule update. Didnt called until the next update
+    private static void _scheduleNextUpdate(Context context, int appWidgetId) {
+        AlarmManager alarmManager =
+                (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        // Substitute AppWidget for whatever you named your AppWidgetProvider subclass
+        Intent intent = new Intent(context, AppWidget.class);
+        intent.setAction(ACTION_SCHEDULED_UPDATE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        Calendar schedule = Calendar.getInstance();
+        //Here is the example how to use prefs for the every widget
+       // int period = ConfigureActivity.loadPeriodFromPref(context, appWidgetId);
+        int period = ConfigureActivity.loadPeriodFromPref(context);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, schedule.getTimeInMillis() + period, pendingIntent);
+        Log.d(AppWidget.class.getCanonicalName() + "._scheduleNextUpdate", "Schedule for updating is set. The next update for the widget ID " + appWidgetId + " will be done in " + period + "ms");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+            _scheduleNextUpdate(context, appWidgetId);
+
+            Log.d(AppWidget.class.getCanonicalName() + ".onUpdate", "Update completed for widget ID " + appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        //Todo: to stop updating of deleted widgets, to delete all preferences and to kill the schedule
+        for (int appWidgetId : appWidgetIds) {
+            ConfigureActivity.deletePeriodFromPref(context, appWidgetId);
+            Log.d(AppWidget.class.getCanonicalName() + ".onDeleted", "Widget ID " + appWidgetId + " is deleted");
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+        Log.d(AppWidget.class.getCanonicalName() + ".onEnabled", "The first widget is created");
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+        Log.d(AppWidget.class.getCanonicalName() + ".onDisabled", "The last widget is disabled");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(ACTION_SCHEDULED_UPDATE)) {
+            Log.d(AppWidget.class.getCanonicalName() + ".onReceive", "The time to update has come");
+            AppWidgetManager manager = AppWidgetManager.getInstance(context);
+            int[] ids = manager.getAppWidgetIds(new ComponentName(context, AppWidget.class));
+            //todo: why to call onUpdate if we have scheduled update? To remove code from upd
+            onUpdate(context, manager, ids);
+        }
+
+        super.onReceive(context, intent);
+    }
+
+}
---
+++ app/src/main/java/com/github/ovorobeva/wordstostudy/ConfigureActivity.java
@@ -0,0 +1,159 @@
+package com.github.ovorobeva.wordstostudy;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+/**
+ * The configuration screen for the {@link AppWidget NewAppWidget} AppWidget.
+ */
+public class ConfigureActivity extends Activity {
+    //todo: to get rid of excess comments
+    //todo: to make logger
+    //path to the prefs on the device
+    static final String PREFS_NAME = "com.github.ovorobeva.wordstostudy.NewAppWidget";
+    //prefix to show with the setting name to separate settings for more than one widgets
+    // /data/user/0/com.github.ovorobeva.wordstostudy/shared_prefs/com.github.ovorobeva.wordstostudy.NewAppWidget.xml
+    static final String PREF_PREFIX_KEY = "appwidget_";
+
+    private static final int EVERY_DAY = 5000;
+    private static final int EVERY_THREE_DAYS = 10000;
+    private static final int EVERY_MONDAY = 15000;
+    private static int count = 3;
+
+    int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    //todo: add other options to save in configurations (period, color, size, languages etc.)
+
+    private int period;
+
+
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            final Context context = ConfigureActivity.this;
+            TextView appwidget_text = findViewById(R.id.appwidget_text);
+            count = Integer.parseInt(appwidget_text.getText().toString());
+
+            // When the button is clicked, store the string locally
+            //todo: to rewrite getWords() to throw the API-request and remove it into update
+            //savePeriodToPref(context, mAppWidgetId, period); The example how to use widget id in prefs
+            savePeriodToPref(context, period);
+            saveWordsCountToPref(context, count);
+
+            // It is the responsibility of the configuration activity to update the app widget
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            AppWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+
+            // Make sure we pass back the original appWidgetId
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+            setResult(RESULT_OK, resultValue);
+            finish();
+        }
+    };
+
+    public ConfigureActivity() {
+        super();
+    }
+
+    // Write the prefix to the SharedPreferences object for this widget
+    //The example how to make prefs for the definite widget (using widgetID)
+/*    static void savePeriodToPref(Context context, int appWidgetId, int period) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putInt(PREF_PREFIX_KEY + appWidgetId, period);
+        prefs.apply();
+    }*/
+
+    static void savePeriodToPref(Context context, int period) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putInt(PREF_PREFIX_KEY, period);
+        prefs.apply();
+    }
+    static void saveWordsCountToPref(Context context, int count) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putInt(PREF_PREFIX_KEY, count);
+        prefs.apply();
+    }
+    // Read the prefix from the SharedPreferences object for this widget.
+    // If there is no preference saved, get the default from a resource
+    //todo: to load other preferences like title value
+    static int loadPeriodFromPref(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        //todo: to make default not an every day. How to place null instead?
+        return prefs.getInt(PREF_PREFIX_KEY, EVERY_DAY);
+    }
+
+    //Here is the example how to use prefs according to the definite widget id
+  /*  static int loadPeriodFromPref(Context context, int mAppWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        return prefs.getInt(PREF_PREFIX_KEY + mAppWidgetId, EVERY_DAY);
+    }*/
+    static int loadWordsCountFromPref(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        //todo: to make default not an every day. How to place null instead?
+        return prefs.getInt(PREF_PREFIX_KEY, count);
+    }
+
+    //todo: to delete other preferences like title value
+    static void deletePeriodFromPref(Context context, int appWidgetId) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.remove(PREF_PREFIX_KEY + appWidgetId);
+        prefs.apply();
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // Set the result to CANCELED.  This will cause the widget host to cancel
+        // out of the widget placement if the user presses the back button.
+        setResult(RESULT_CANCELED);
+
+        setContentView(R.layout.widget_configure);
+
+        findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
+
+        // Find the widget id from the intent.
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        // If this activity was started with an intent without an app widget ID, finish with an error.
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+            return;
+        }
+//todo: to set other options as default
+        period = loadPeriodFromPref(ConfigureActivity.this);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //todo: to restore the previous state (to watch the lesson again)
+        Log.d(AppWidget.class.getCanonicalName() + ".onResume", "Config activity for the widget ID " + mAppWidgetId + " is opened");
+
+    }
+
+    public void radioButtonClickListener(View view) {
+        RadioButton checkedRadioButton = (RadioButton) view;
+        switch (checkedRadioButton.getId()) {
+            case R.id.every_day:
+                period = EVERY_DAY;
+                break;
+            case R.id.every_three_days:
+                period = EVERY_THREE_DAYS;
+                break;
+            case R.id.every_monday:
+                period = EVERY_MONDAY;
+        }
+    }
+}
